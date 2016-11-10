59434c5e299558b05f949b1a45cd8fb52f972b57
Deniz Ozger <deniz.ozger@velocityapp.com>
2016-11-10 14:40:47 +0000

    New listing rules and fixing broken lines

--- test/test.js
+++ test/test.js
@@ -1,39 +1,25 @@
-import assert from 'assert';
-import sinon from 'sinon';
-import log from '../lib/menna';
+/* eslint-env node*/
+
+import assert from 'assert'
+import sinon from 'sinon'
+import log from '../lib/menna'
 
 describe('menna', () => {
+  const LOGGING_LEVELS = ['debug', 'info', 'warn', 'error']
 
-  const loggingLevels = ['debug', 'info', 'warn', 'error'];
-
-  before(() => {
-    for (let level of loggingLevels) {
-      sinon.stub(log, level).returns(void 0);
-    }
-  });    
-
-  describe('should log', () => {
-
-    for (let level of loggingLevels) {
-      it(`in ${level} level`, () => {
-        testLoggingLevel(level);
-      });
-    }
-
-  });
+  before(() => LOGGING_LEVELS.forEach(level => sinon.stub(log, level).returns(undefined)))
 
   function testLoggingLevel(level) {
-    let message = `${level} message`; 
-    log[level](message);
-    assert(log[level].called);
-    assert.equal(log[level].callCount, 1);
-    assert.equal(log[level].getCall(0).args[0], message);
+    it(`in ${level} level`, () => {
+      const message = `${level} message`
+      log[level](message)
+      assert(log[level].called)
+      assert.equal(log[level].callCount, 1)
+      assert.equal(log[level].getCall(0).args[0], message)
+    })
   }
 
-  after(() => {
-    for (let level of loggingLevels) {
-      log[level].restore();
-    }
-  });
+  describe('should log', () => LOGGING_LEVELS.forEach(testLoggingLevel))
 
-});
+  after(() => LOGGING_LEVELS.forEach(level => log[level].restore()))
+})
